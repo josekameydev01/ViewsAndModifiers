4102ff1dae01dc8ad8a1971b9c4fe057a761377c
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-19 11:53:10 -0600

    First commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by carlosgalvankamey on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
