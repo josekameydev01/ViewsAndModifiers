756115fd9b7530daac73e993371bbad3ce18370b
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-22 22:42:01 -0600

    views as properties

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,17 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var colorText = true
-    var body: some View {
-        VStack(spacing: 15) {
-            Text("Hello, Swift1!")
-                .blur(radius: 1)
-            Text("Hello, Swift1!")
-            Text("Hello, Swift1!")
-            Text("Hello, Swift1!")
-            Text("Hello, Swift1!")
+    let text1 = Text("Hello, Swift!")
+    let text2 = Text("Hello, SwiftUI!")
+    
+    var text3: some View {
+        VStack {
+            text1
+                .font(.largeTitle.bold())
+                .foregroundStyle(.green)
+            text2
+                .font(.largeTitle.bold())
+                .foregroundStyle(.purple)
         }
-        .blur(radius: 1)
+    }
+    
+    var text4: some View {
+        Group {
+            text1
+                .foregroundStyle(.pink)
+            text2
+                .foregroundStyle(.cyan)
+        }
+    }
+    
+    @ViewBuilder var text5: some View {
+        text1
+            .font(.largeTitle.bold())
+            .foregroundStyle(.yellow)
+        text2
+            .font(.largeTitle.bold())
+            .foregroundStyle(.orange)
+    }
+    
+    var body: some View {
+        VStack {
+            text1
+                .font(.largeTitle.bold())
+                .foregroundStyle(.red)
+            text2
+                .font(.largeTitle.bold())
+                .foregroundStyle(.blue)
+            text3
+            HStack {
+                text4
+            }
+            text5
+        }
     }
 }
 
